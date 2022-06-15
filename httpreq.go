27caf776505c174f1b0056b4d3b873e4a5fd59cf
p518 <f8192@ya.ru>
2022-06-16 01:45:37 +0400

    Move http functions

--- httpreq.go
+++ httpreq.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"./settings"
+	"io/ioutil"
+	"fmt"
 )
+
+
+func ginInit() {
+	gin.SetMode(gin.ReleaseMode); //disable debug logs
+	gin.DefaultWriter = ioutil.Discard; //disable output
+	r := gin.Default();
+	r.MaxMultipartMemory = 1 << 20;
+
+	r.GET("/ping", HttpReqPing);
+	
+	fmt.Printf("Starting web server\n");
+	go r.Run(":"+settings.ListenPort); //Listen on port
+}
+
 
 
 func HttpReqPing(c *gin.Context) {
--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	//"database/sql"
-	"io/ioutil"
 	//_ "github.com/lib/pq"
-	"github.com/gin-gonic/gin"
 	"./settings"
 )
 
@@ -51,15 +49,3 @@ func main() {
 	return true;
 }*/
 
-
-func ginInit() {
-	gin.SetMode(gin.ReleaseMode); //disable debug logs
-	gin.DefaultWriter = ioutil.Discard; //disable output
-	r := gin.Default();
-	r.MaxMultipartMemory = 1 << 20;
-
-	r.GET("/ping", HttpReqPing);
-	
-	fmt.Printf("Starting web server\n");
-	go r.Run(":"+settings.ListenPort); //Listen on port
-}
