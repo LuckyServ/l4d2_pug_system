39a988d37c3edb8b3aa6e7625c15f5cadc872da8
sheo9000 <aaaaa@aaaaa.aa>
2022-11-07 00:55:32 +0400

    Revert prev commit (wasnt the issue)

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"../settings"
+	"github.com/remerge/gzip"
 )
 
 
@@ -12,7 +13,7 @@ func GinInit() {
 	gin.SetMode(gin.ReleaseMode); //disable debug logs
 	gin.DefaultWriter = ioutil.Discard; //disable output
 	r := gin.Default();
-	//r.Use(gzip.Gzip(gzip.DefaultCompression));
+	r.Use(gzip.Gzip(gzip.DefaultCompression));
 	r.MaxMultipartMemory = 1 << 20;
 
 	r.GET("/shutdown", HttpReqShutdown);
