1fb62b821ca9bfdd7ec543f757f67d905153314e
sheo9000 <aaaaa@aaaaa.aa>
2022-12-30 23:39:45 +0400

    Im stupid

--- api/steamid32to64.go
+++ api/steamid32to64.go
@@ -13,10 +13,12 @@ func HttpSteamID32to64(c *gin.Context) {
 
 	var sResponse string;
 
+	sSteamID32 := c.Query("steamid32");
+
 	bSteamID32Valid, _ := regexp.MatchString(`^STEAM_[0-5]:[01]:\d+$`, c.Query("steamid32"));
 	if (bSteamID32Valid) {
 
-		arSteamID32 := strings.Split(c.PostForm("steamid"), ":");
+		arSteamID32 := strings.Split(sSteamID32, ":");
 		iY, _ := strconv.ParseInt(arSteamID32[1], 10, 64);
 		iZ, _ := strconv.ParseInt(arSteamID32[2], 10, 64);
 		sSteamID64 := fmt.Sprintf("%d", 0x0110000100000000 + (iZ * 2) + iY);
