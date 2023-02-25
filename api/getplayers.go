3ee86fae24598e2023c894aab7812e5050df650b
sheo9000 <aaaaa@aaaaa.aa>
2023-02-25 16:16:42 +0400

    mmr_uncertainty in playerlist

--- api/getplayers.go
+++ api/getplayers.go
@@ -14,6 +14,7 @@ type PlayerResponse struct {
 	NicknameBase64			string		`json:"nickname_base64"`
 	AvatarSmall				string		`json:"avatar_small"`
 	Mmr						int			`json:"mmr"`
+	MmrUncertainty			float32		`json:"mmr_uncertainty"`
 	Access					int 		`json:"access"` //-2 - completely banned, -1 - chat banned, 0 - regular player, 1 - behaviour moderator, 2 - cheat moderator, 3 - behaviour+cheat moderator, 4 - full admin access
 	IsInGame				bool		`json:"is_ingame"`
 	MmrGrade				int			`json:"mmr_grade"`
@@ -25,6 +26,7 @@ type PlayerResponseMe struct {
 	AvatarSmall				string		`json:"avatar_small"`
 	AvatarBig				string		`json:"avatar_big"`
 	Mmr						int			`json:"mmr"`
+	MmrUncertainty			float32		`json:"mmr_uncertainty"`
 	Access					int 		`json:"access"` //-2 - completely banned, -1 - chat banned, 0 - regular player, 1 - behaviour moderator, 2 - cheat moderator, 3 - behaviour+cheat moderator, 4 - full admin access
 	BanReason				string 		`json:"banreason"`
 	BanAcceptedAt			int64 		`json:"ban_accepted_at"`
@@ -65,6 +67,7 @@ func HttpReqGetPlayers(c *gin.Context) {
 				AvatarSmall:			pPlayer.AvatarSmall,
 				AvatarBig:				pPlayer.AvatarBig,
 				Mmr:					pPlayer.Mmr,
+				MmrUncertainty:			pPlayer.MmrUncertainty,
 				Access:					pPlayer.Access,
 				BanReason:				pPlayer.BanReason,
 				BanAcceptedAt:			pPlayer.BanAcceptedAt,
@@ -154,6 +157,7 @@ func AppendPlayerResponse(arPlayers []PlayerResponse, pPlayer *players.EntPlayer
 		NicknameBase64:			pPlayer.NicknameBase64,
 		AvatarSmall:			pPlayer.AvatarSmall,
 		Mmr:					pPlayer.Mmr,
+		MmrUncertainty:			pPlayer.MmrUncertainty,
 		Access:					pPlayer.Access,
 		IsInGame:				pPlayer.IsInGame,
 		MmrGrade:				players.GetMmrGrade(pPlayer),
